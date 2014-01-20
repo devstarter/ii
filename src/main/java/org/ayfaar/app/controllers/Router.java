1d9267e7b456498ae2a4576211b8e22a01a78fdd
Lebid Yuriy <ylebid@gmail.com>
2014-01-20 13:53:29 +0200

    ii

--- src/main/java/org/ayfaar/app/controllers/Router.java
+++ src/main/java/org/ayfaar/app/controllers/Router.java
@@ -1,23 +1,28 @@
 package org.ayfaar.app.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class Router {
-    @Autowired ServletContext context;
+//    @Autowired ServletContext context;
 //    private final DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+    @Value("${OPENSHIFT_JBOSSEWS_DIR}")
+    private String jbossDir;
 
     @RequestMapping("/")
     @ResponseBody
     public FileSystemResource returnIndex(HttpServletRequest request) {
-        return new FileSystemResource(request.getServletContext().getRealPath("index.html"));
+        String path = request.getServletContext().getRealPath("index.html");
+        if (path == null) {
+            path = jbossDir + "webapps/index.html";
+        }
+        return new FileSystemResource(path);
     }
 
     /*@RequestMapping("/")
