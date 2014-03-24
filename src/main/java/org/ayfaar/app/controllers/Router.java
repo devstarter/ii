2b7a34d1508705ce9bd9a2735518d2a3d7b37307
Lebid Yuriy <ylebid@gmail.com>
2014-03-24 18:24:15 +0200

    dgdg

--- src/main/java/org/ayfaar/app/controllers/Router.java
+++ src/main/java/org/ayfaar/app/controllers/Router.java
@@ -1,32 +1,51 @@
 package org.ayfaar.app.controllers;
 
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
 
-//@Controller
+@Controller
 public class Router {
 //    @Autowired ServletContext context;
     private final DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-//    @Value("${OPENSHIFT_JBOSSEWS_DIR}")
-//    private String jbossDir;
+    @Value("${OPENSHIFT_JBOSSEWS_DIR}")
+    private String jbossDir;
 
     @RequestMapping("/")
     @ResponseBody
     public Object returnIndex(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String path = request.getServletContext().getRealPath("ii");
-        if (path != null) {
-            return new FileSystemResource(path);
-        } else {
-            redirectStrategy.setContextRelative(true);
-            redirectStrategy.sendRedirect(request, response, "ii");
-            return null;
+//        String path = request.getServletContext().getRealPath("ii");
+//        if (path != null) {
+//            return new FileSystemResource(path);
+//        } else {
+//            redirectStrategy.setContextRelative(true);
+//            redirectStrategy.sendRedirect(request, response, "ii");
+//            return null;
+//        }
+        File baseDir = new File(jbossDir);
+
+        return find(baseDir);
+    }
+
+    private String find(File dir) {
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                String result = find(file);
+                if (!result.equals("not found")) {
+                    return result;
+                }
+            } else if (file.getName().equals("index.html")) {
+                return file.getAbsolutePath();
+            }
         }
+        return "not found";
     }
 
     /*@RequestMapping("/")
