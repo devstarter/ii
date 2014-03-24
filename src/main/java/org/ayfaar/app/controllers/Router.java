44617fa9fd1915442214af8ef5aa3a9248c21e0e
Lebid Yuriy <ylebid@gmail.com>
2014-03-24 20:22:33 +0200

    test

--- src/main/java/org/ayfaar/app/controllers/Router.java
+++ src/main/java/org/ayfaar/app/controllers/Router.java
@@ -1,11 +1,14 @@
 package org.ayfaar.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
@@ -13,7 +16,7 @@ import java.io.IOException;
 
 @Controller
 public class Router {
-//    @Autowired ServletContext context;
+    @Autowired ServletContext context;
     private final DefaultRedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
     @Value("${OPENSHIFT_HOMEDIR}")
     private String jbossDir;
@@ -21,17 +24,15 @@ public class Router {
     @RequestMapping("/")
     @ResponseBody
     public Object returnIndex(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        String path = request.getServletContext().getRealPath("ii");
-//        if (path != null) {
-//            return new FileSystemResource(path);
-//        } else {
-//            redirectStrategy.setContextRelative(true);
-//            redirectStrategy.sendRedirect(request, response, "ii");
-//            return null;
-//        }
-        File baseDir = new File(jbossDir);
+        String index = "index.html";
+        String path = request.getServletContext().getRealPath(index);
+        if (path == null) {
+            path = jbossDir+"app-deployments/current/repo/src/main/webapp/"+index;
+        }
+        return new FileSystemResource(path);
 
-        return find(baseDir);
+//        File baseDir = new File(jbossDir);
+//        return find(baseDir);
     }
 
     private String find(File dir) {
