9900a52ded5dfe8a0c4bcd496b84f7cf78a3cbbd
Lebid Yuriy <ylebid@gmail.com>
2014-07-11 23:01:23 +0300

    issue2

---
+++ src/test/java/issues/issue2/Issue2IntegrationTest.java
@@ -0,0 +1,27 @@
+package issues.issue2;
+
+import org.ayfaar.app.IntegrationTest;
+import org.ayfaar.app.dao.ItemDao;
+import org.ayfaar.app.model.Item;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.junit.Assert.assertEquals;
+
+public class Issue2IntegrationTest extends IntegrationTest {
+
+    @Value("#{T(org.apache.commons.io.FileUtils).readFileToString(" +
+            "T(org.springframework.util.ResourceUtils).getFile('expected-item-3.0089.txt')" +
+            ")}")
+    String itemExpectedContent;
+
+    @Autowired ItemDao itemDao;
+
+    @Test
+    public void checkParticularItem() {
+        Item item = itemDao.getByNumber("3.0089");
+        assertEquals(itemExpectedContent, item.getContent());
+    }
+
+}
--- src/test/java/org/ayfaar/app/IntegrationTest.java
+++ src/test/java/org/ayfaar/app/IntegrationTest.java
@@ -1,22 +1,17 @@
-import org.ayfaar.app.SpringTestConfiguration;
-import org.ayfaar.app.dao.ItemDao;
-import org.ayfaar.app.model.Item;
-import org.junit.Test;
+package org.ayfaar.app;
+
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringTestConfiguration.class)
 public class IntegrationTest {
-    @Autowired ItemDao itemDao;
+    /*@Autowired ItemDao itemDao;
 
     @Test
     public void integrationTest() {
         Item item = itemDao.getByNumber("1.0001");
         assertNotNull(item);
-    }
+    }*/
 }
