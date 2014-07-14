57c70c6cf181badf45cfb47cf5a079e753601ec0
Ekvi <dima_sh84@mail.ru>
2014-07-15 00:08:21 +0300

    issue2  add some unit tests

--- src/main/java/org/ayfaar/app/utils/ItemsCleaner.java
+++ src/main/java/org/ayfaar/app/utils/ItemsCleaner.java
@@ -1,10 +1,12 @@
 package org.ayfaar.app.utils;
 
-import org.apache.commons.lang.NotImplementedException;
-
 public class ItemsCleaner {
     public static String clean(String value) {
-        // fixme: issue#2
-        throw new NotImplementedException("issue#2");
+        String[] str = value.split("\n");
+
+        if(value.length() == 0) {
+            return null;
+        }
+        return str[0];
     }
 }
--- src/test/java/issues/issue2/Issue2UnitTest.java
+++ src/test/java/issues/issue2/Issue2UnitTest.java
@@ -2,22 +2,45 @@ package issues.issue2;
 
 import org.apache.commons.io.IOUtils;
 import org.ayfaar.app.utils.ItemsCleaner;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class Issue2UnitTest {
+    private String valueWithBug;
+    private String expectedValue;
+
+    @Before
+    public void init() throws IOException {
+        valueWithBug = IOUtils.toString(Issue2UnitTest.class.getResourceAsStream("dirty-item-3.0089.txt"));
+        expectedValue = IOUtils.toString(Issue2UnitTest.class.getResourceAsStream("clean-item-3.0089.txt"));
+    }
 
     @Test
     public void test1() throws IOException {
-        String valueWithBug = IOUtils.toString(Issue2UnitTest.class.getResourceAsStream("clean-item-3.0089.txt"));
-        String expectedValue = IOUtils.toString(Issue2UnitTest.class.getResourceAsStream("dirty-item-3.0089.txt"));
         String actualValue = ItemsCleaner.clean(valueWithBug);
         assertEquals(expectedValue, actualValue);
     }
 
-    // todo: проверить на null, пустую строку
-    // todo: придумать ещё варианты тестов :)
+    @Test
+    public void cleanValueNotNullTest() throws IOException {
+        String actualValue = ItemsCleaner.clean(valueWithBug);
+        assertNotNull(actualValue);
+    }
+
+    @Test
+    public void emptyString() throws IOException {
+        valueWithBug = IOUtils.toString(Issue2UnitTest.class.getResourceAsStream("empty-item-3.0089.txt"));
+        String actualValue = ItemsCleaner.clean(valueWithBug);
+        assertNull(actualValue);
+    }
+
+    @Test
+    public void equalityStringLength() {
+        int actualLength = ItemsCleaner.clean(valueWithBug).length();
+        assertEquals(expectedValue.length(), actualLength);
+    }
 }
