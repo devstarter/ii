0d78fdbbd74c939cc6c823aa341e44ef865dfc1e
Lebid Yuriy <ylebid@gmail.com>
2014-07-18 17:23:00 +0300

    add description

--- src/main/java/org/ayfaar/app/controllers/search/Suggestion.java
+++ src/main/java/org/ayfaar/app/controllers/search/Suggestion.java
@@ -1,4 +1,11 @@
 package org.ayfaar.app.controllers.search;
 
+import lombok.Data;
+
+/**
+ * Клас для хранения и сериализации в json значения предложения
+ */
+@Data
 public class Suggestion {
+    private String label;
 }
