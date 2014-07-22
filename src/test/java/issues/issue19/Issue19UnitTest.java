18a1a7edb2fc09942664a907e1938bd27f8fac18
Lebid Yuriy <ylebid@gmail.com>
2014-07-22 23:56:50 +0300

    improve issue19

--- src/test/java/issues/issue19/Issue19UnitTest.java
+++ src/test/java/issues/issue19/Issue19UnitTest.java
@@ -6,33 +6,43 @@ import org.ayfaar.app.model.Term;
 import org.ayfaar.app.services.SearchService;
 import org.ayfaar.app.utils.AliasesMap;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static net.sf.cglib.core.CollectionUtils.transform;
 import static org.junit.Assert.assertEquals;
-import static org.springframework.test.util.ReflectionTestUtils.setField;
 
+/**
+ * о анотациях http://stackoverflow.com/q/20270391/975169
+ */
+@RunWith(MockitoJUnitRunner.class)
 public class Issue19UnitTest {
+    @Mock AliasesMap aliasesMap;
+
+    @InjectMocks @Spy
+    SearchService searchService = new SearchService();
+
+    @InjectMocks
+    SearchController2 controller = new SearchController2();
+
     @Test
     public void testSequence() {
         String q = "a";
         List<String> fakeTerms = asList("a", "1 a", "bterfd", "b-aaaa", "aa", "242a424");
-        AliasesMap fakeAliasesMap = Mockito.mock(AliasesMap.class);
         //noinspection unchecked
-        Mockito.when(fakeAliasesMap.getAllTerms()).thenReturn(transform(fakeTerms, new Transformer() {
+        Mockito.when(aliasesMap.getAllTerms()).thenReturn(transform(fakeTerms, new Transformer() {
             @Override
             public Object transform(Object value) {
                 return new Term((String) value);
             }
         }));
-        SearchService searchService = new SearchService();
-        setField(searchService, "aliasesMap", fakeAliasesMap);
-
-        SearchController2 controller = new SearchController2();
-        setField(controller, "searchService", searchService);
 
         List<String> suggestions = controller.suggestions(q);
 
