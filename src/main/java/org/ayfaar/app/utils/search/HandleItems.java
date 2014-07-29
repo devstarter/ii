83c5ea545324e6c59cc57d1494a63a88098c9240
Ekvi <dima_sh84@mail.ru>
2014-07-29 16:33:12 +0300

    update HandleItems class

--- src/main/java/org/ayfaar/app/utils/search/HandleItems.java
+++ src/main/java/org/ayfaar/app/utils/search/HandleItems.java
@@ -1,23 +1,42 @@
 package org.ayfaar.app.utils.search;
 
+import lombok.Data;
+import org.ayfaar.app.controllers.NewSearchController;
 import org.ayfaar.app.model.Item;
+import org.springframework.beans.factory.annotation.Autowired;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import org.ayfaar.app.controllers.NewSearchController.Quote;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //Обработка найденных пунктов
+@Data
 public class HandleItems {
+    @Autowired
+    private NewSearchController controller;
+
     private List<Item> foundedItems;
     private String requiredPhrase;
 
-    public HandleItems(List<Item> items, String phrase) {
-        foundedItems = items;
-        requiredPhrase = phrase;
-    }
-
     public List<Quote> createQuotes(List<Item> foundedItems) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+
+        List<Quote> quotes = new ArrayList<Quote>();
+        for(Item i : foundedItems) {
+            Quote quote = controller.new Quote();
+            quote.setUri(i.getUri());
+            quote.setQuote(i.getContent());
+            quotes.add(quote);
+        }
+        quotes = changeSentenceWithRequiredPhrase(quotes);
+
+        for(int i = 0; i < quotes.size(); i++) {
+            Quote quote = controller.new Quote();
+            quote.setQuote(decorateRequiredPhrase(quotes.get(i).getQuote()));
+            quotes.set(i, quote);
+        }
+        return quotes;
     }
 
     public List<Quote> changeSentenceWithRequiredPhrase(List<Quote> quotes) {
