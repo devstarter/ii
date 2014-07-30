9d662350074650df2b9066651d4a4cfdf30e5432
Ekvi <dima_sh84@mail.ru>
2014-07-30 10:22:49 +0300

    del controller in HandleItems class

--- src/main/java/org/ayfaar/app/controllers/search/HandleItems.java
+++ src/main/java/org/ayfaar/app/controllers/search/HandleItems.java
@@ -1,9 +1,7 @@
 package org.ayfaar.app.controllers.search;
 
 import org.apache.commons.lang.NotImplementedException;
-import org.ayfaar.app.controllers.NewSearchController;
 import org.ayfaar.app.model.Item;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,9 +9,6 @@ import java.util.List;
 //5.Обработка найденных пунктов
 @Component
 public class HandleItems {
-    @Autowired
-    private NewSearchController controller;
-
     public static final int sentenceMaxWords = 30;
 
     public List<Quote> createQuotes(List<Item> foundedItems, List<String> allPossibleSearchQueries) {
