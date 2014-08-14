f15f5162ebcb7947cf09b839ed9b28932619f856
DyadyaMitya <dimonpavlo@yahoo.com>
2014-08-14 16:29:16 +0300

    46 changed

--- src/main/java/org/ayfaar/app/controllers/search/SearchCacheImpl.java
+++ src/main/java/org/ayfaar/app/controllers/search/SearchCacheImpl.java
@@ -1,49 +1,38 @@
 package org.ayfaar.app.controllers.search;
 
-import com.sun.org.apache.bcel.internal.generic.NOP;
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Created by Dim on 09.08.2014.
  */
 public class SearchCacheImpl implements SearchCache{
-    private Map<Object, SearchResultPage> myCache;
+    private Map<Object, SearchResultPage> library;
 
     public SearchCacheImpl() {
-        myCache = new HashMap<Object, SearchResultPage>();
+        library = new HashMap<Object, SearchResultPage>();
     }
 
     @Override
-    public Object generateKey(String query, Integer pageNumber, String fromItemNumber) throws NullPointerException{
-        String cacheKey = "";
-        try {
-            cacheKey = query + pageNumber + fromItemNumber;
-            return cacheKey;
-        }catch (NullPointerException e){
-            cacheKey = query + pageNumber + "null";
-            return cacheKey;
-        }
+    public Object generateKey(String query, Integer pageNumber, String fromItemNumber){
+        if (fromItemNumber == null) return query + pageNumber.toString() + "null";
+        return query + pageNumber.toString() + fromItemNumber;
     }
 
     @Override
     public boolean has(Object cacheKey) {
-        if (myCache.containsKey(cacheKey) && !myCache.get(cacheKey).equals(null)) return true;
-        return false;
+        return library.containsKey(cacheKey) && library.get(cacheKey) != null;
     }
 
     @Override
     public SearchResultPage get(Object cacheKey){
-        if (cacheKey.equals(null)) return null;
-        return myCache.get(cacheKey);
+        return library.get(cacheKey);
     }
 
     @Override
     public void put(Object cacheKey, SearchResultPage page){
         if (cacheKey == null) throw new IllegalArgumentException();
         if (page == null) throw new IllegalArgumentException();
-        myCache.put(cacheKey, page);
+        library.put(cacheKey, page);
     }
 }
