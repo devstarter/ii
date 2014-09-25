1159e3f4d89bed207fda2f2adcbd895bba328927
Lebid Yuriy <ylebid@gmail.com>
2014-09-25 12:14:08 +0300

    +

--- src/test/java/org/ayfaar/app/format/FormatItemsTest.java
+++ src/test/java/org/ayfaar/app/format/FormatItemsTest.java
@@ -11,6 +11,10 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ *  Для тестирования возьмём 10 том в формате html, который лежит в ресурсах к тесту
+ * (src/test/resources/org/ayfaar/app/format/10tom.html).
+ */
 public class FormatItemsTest extends AbstractTest {
     private static Document doc;
     private final String html10_10001;
@@ -25,6 +29,9 @@ public class FormatItemsTest extends AbstractTest {
     }
 
     @Test
+    /*
+    тест не проходит так как нужно убрать обрамления пробелов (<span class="char-style-override-4"> </span>)
+     */
     public void first() throws IOException {
         Element item = getItemHtmlElement("10.10001");
         String number = item.text();
@@ -33,7 +40,8 @@ public class FormatItemsTest extends AbstractTest {
         String formatted = FormatItems.format(item);
         assertEquals(html10_10001, formatted);
         String unformatted = Jsoup.clean(formatted, Whitelist.none());
-        // HtmlCharacterEntityDecoder
+        // нужно помимо очистки от тегов приобразовать коды типа &laquo; в текстовое представление типа "
+        // возможно поможет клас HtmlCharacterEntityDecoder
         assertEquals(text10_10001, unformatted);
     }
 
@@ -48,7 +56,8 @@ public class FormatItemsTest extends AbstractTest {
 
     }
 
+    // todo написать тесты для этих случаев
     // убрать ссылки на http://www.ayfaar.org/wiki/... например в пункте 10.10031.
     // оставить италик напрмер <span class="italic">informare»</span> в 10.10042.
-    // оставить италик и подчёркнутый напрмер <span class="italic-und">недискретного</span> в 10.10083.
+    // оставить италик и подчёркнутый например <span class="italic-und">недискретного</span> в 10.10083.
 }
