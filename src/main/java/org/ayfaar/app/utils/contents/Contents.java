9f302a93ef4bb1c67230f2765c6951d317d77b82
Ekvi <dima_sh84@mail.ru>
2014-10-08 17:40:55 +0300

    added method createContents

--- src/main/java/org/ayfaar/app/utils/contents/Contents.java
+++ src/main/java/org/ayfaar/app/utils/contents/Contents.java
@@ -1,7 +1,10 @@
 package org.ayfaar.app.utils.contents;
 
+import org.ayfaar.app.controllers.ItemController;
 import org.ayfaar.app.dao.CategoryDao;
+import org.ayfaar.app.dao.ItemDao;
 import org.ayfaar.app.model.Category;
+import org.ayfaar.app.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,23 +15,44 @@ import java.util.List;
 public class Contents {
     @Autowired
     private CategoryDao categoryDao;
+    @Autowired
+    private ItemDao itemDao;
+
+    public List<String> createContents(Category category) {
+        List<Category> subCategories = getSubCategories(category);
+        List<String> contents = new ArrayList<String>();
+
+        if(subCategories.get(0).isParagraph()) {
+            for(Category c : subCategories) {
+                contents.add(formatParagraph(c));
+                contents.addAll(getItems(c));
+            }
+        }
+        else {
+            for(Category c : subCategories) {
+                if(c.isParagraph()) {
+                    contents.add(formatParagraph(c));
+                }
+                else {
+                    contents.add(formatSectionAndChapter(c));
+                }
+            }
+        }
+
+        return contents;
+    }
 
     public List<Category> getSubCategories(Category category) {
         List<Category> categories = getChildren(category);
+        List<Category> subCategories = new ArrayList<Category>();
 
-        print(categories);
-
-        return categories;
-    }
-
-
-
-    private void print(List<Category> categories) {
         for(Category c : categories) {
-            System.out.println(c.getName());
+            subCategories.add(c);
+            subCategories.addAll(getChildren(c));
         }
-    }
 
+        return subCategories;
+    }
 
     private List<Category> getChildren(Category parent) {
         List<Category> children = new ArrayList<Category>();
@@ -45,5 +69,39 @@ public class Contents {
             }
         }
         return children;
+    }
+
+    private List<String> getItems(Category category) {
+        List<String> itemContents = new ArrayList<String>();
+
+        Item currentItem = itemDao.get(category.getStart());
+        String itemNumber = currentItem.getNumber();
+        String endNumber = null;
+        if (category.getEnd() != null) {
+            endNumber = itemDao.get(category.getEnd()).getNumber();
+        }
+
+        itemContents.add(formatItem(currentItem));
+        while(!itemNumber.equals(endNumber)) {
+            itemNumber = ItemController.getNext(itemNumber);
+            currentItem = itemDao.getByNumber(itemNumber);
+            itemContents.add(formatItem(currentItem));
+        }
+
+        return itemContents;
+    }
+
+    public String formatSectionAndChapter(Category category) {
+        String[] names = category.getName().split("/");
+        String line = names[names.length-1].trim() + ". " + category.getDescription();
+        return  line;
+    }
+
+    public String formatParagraph(Category category) {
+        return  category.getName().replace("Параграф ", "§") + " " + category.getDescription();
+    }
+
+    public String formatItem(Item item) {
+        return item.getNumber() + ". " + item.getContent();
     }
 }
--- src/test/java/issues/issue63/TestContents.java
+++ src/test/java/issues/issue63/TestContents.java
@@ -1,62 +1,142 @@
 package issues.issue63;
 
 
+import org.apache.commons.io.IOUtils;
 import org.ayfaar.app.IntegrationTest;
 import org.ayfaar.app.dao.CategoryDao;
+import org.ayfaar.app.dao.ItemDao;
 import org.ayfaar.app.model.Category;
+import org.ayfaar.app.model.Item;
 import org.ayfaar.app.utils.contents.Contents;
 import org.hibernate.criterion.MatchMode;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.List;
 
 public class TestContents extends IntegrationTest {
     @Autowired
-    Contents contents;
+    private Contents contents;
 
     @Autowired
-    CategoryDao categoryDao;
+    private CategoryDao categoryDao;
+
+    @Autowired
+    private ItemDao itemDao;
+
+    private String[] subCategoryTom10;
+    private String[] subCategorySectionII;
+
+    @Before
+    public void init() throws IOException {
+        String tom = IOUtils.toString(this.getClass().getResourceAsStream("subCategoryTom10.txt"));
+        String section = IOUtils.toString(this.getClass().getResourceAsStream("subCategorySectionII.txt"));
+        subCategoryTom10 = tom.split("\n");
+        subCategorySectionII = section.split("\n");
+    }
 
     @Test
-    public void testTom() {
+    public void testGetSubCategoryTom() {
         List<Category> root = categoryDao.getTopLevel();
         List<Category> categories = contents.getSubCategories(root.get(0));
 
-        assertEquals(2, categories.size());
+        assertEquals(11, categories.size());
         assertEquals("Том 10", categories.get(0).getName());
-        assertEquals("Том 14", categories.get(1).getName());
+        assertEquals("БДК / Раздел I", categories.get(1).getName());
+        assertEquals("Том 14", categories.get(7).getName());
+        assertEquals("БДК / Раздел ХVIII", categories.get(10).getName());
     }
 
     @Test
-    public void testSection() {
+    public void testGetSubCategorySection() {
         List<Category> toms = categoryDao.getLike("name", "Том 10", MatchMode.END);
         List<Category> categories = contents.getSubCategories(toms.get(0));
 
-        assertEquals(6, categories.size());
+        assertEquals(30, categories.size());
         assertEquals("БДК / Раздел I", categories.get(0).getName());
-        assertEquals("БДК / Раздел VI", categories.get(5).getName());
+        assertEquals("БДК / Раздел I / Глава 1", categories.get(1).getName());
+        assertEquals("БДК / Раздел VI", categories.get(26).getName());
+        assertEquals("БДК / Раздел VI / Глава 3", categories.get(29).getName());
     }
 
     @Test
-    public void testChapter() {
+    public void testGetSubCategoryChapter() {
         List<Category> sections = categoryDao.getLike("name", "БДК / Раздел I", MatchMode.END);
         List<Category> categories = contents.getSubCategories(sections.get(0));
 
-        assertEquals(4, categories.size());
+        assertEquals(29, categories.size());
         assertEquals("БДК / Раздел I / Глава 1", categories.get(0).getName());
-        assertEquals("БДК / Раздел I / Глава 4", categories.get(3).getName());
+        assertEquals("Параграф 10.1.1.1", categories.get(1).getName());
+        assertEquals("БДК / Раздел I / Глава 2", categories.get(11).getName());
+        assertEquals("Параграф 10.1.2.3", categories.get(14).getName());
     }
 
     @Test
-    public void testParagraph() {
+    public void testGetSubCategoryParagraph() {
         List<Category> chapters = categoryDao.getLike("name", "БДК / Раздел I / Глава 1", MatchMode.END);
         List<Category> categories = contents.getSubCategories(chapters.get(0));
 
         assertEquals(10, categories.size());
         assertEquals("Параграф 10.1.1.1", categories.get(0).getName());
         assertEquals("Параграф 10.1.1.10", categories.get(9).getName());
+    }
+
+    @Test
+    public void testFormatSectionAndChapter() {
+        String expectedSection = "Раздел II. Новейшие духовно-космологические Представления о Вселенной и о человеке";
+        String expectedChapter = "Глава 2. Понятия «Фокусов»";
+        List<Category> sections = categoryDao.getLike("name", "БДК / Раздел II", MatchMode.END);
+        List<Category> chapters = categoryDao.getLike("name", "БДК / Раздел I / Глава 2", MatchMode.END);
+        String section = contents.formatSectionAndChapter(sections.get(0));
+        String chapter = contents.formatSectionAndChapter(chapters.get(0));
+
+        assertEquals(expectedSection, section);
+        assertEquals(expectedChapter, chapter);
+    }
+
+    @Test
+    public void testFormatParagraph() {
+        String expectedParagraph = "§10.1.1.1 О бесконечности Мироздания и Ииссиидиологии, которая призвана в" +
+                " корне изменить  наши взгляды";
+        List<Category> paragraphs = categoryDao.getLike("name", "Параграф 10.1.1.1", MatchMode.END);
+        String paragraph = contents.formatParagraph(paragraphs.get(0));
+
+        assertEquals(expectedParagraph, paragraph);
+    }
+
+    @Test
+    public void testFormatItem() {
+        String expectedItem = "10.10488. В этом сложном и одновременном эволюционно-инволюционном Процессе" +
+                " есть свои особенности и тонкости, первой из которых является наличие принципа большей степени" +
+                " совместимости, избирательности или предпочтительности при инерционном осуществлении Синтеза одних" +
+                " Аспектов Качеств по отношению к Аспектам других Качеств.";
+        List<Item> items = itemDao.getLike("number", "10.10488", MatchMode.END);
+        String item = contents.formatItem(items.get(0));
+
+        assertEquals(expectedItem, item);
+    }
+
+    @Test
+    public void testCreateContents() {
+        List<Category> toms = categoryDao.getLike("name", "Том 10", MatchMode.END);
+        List<Category> sections = categoryDao.getLike("name", "БДК / Раздел II", MatchMode.END);
+        List<Category> chapters = categoryDao.getLike("name", "БДК / Раздел I / Глава 1", MatchMode.END);
+        List<String> tom10Contents = contents.createContents(toms.get(0));
+        List<String> sectionIIContents = contents.createContents(sections.get(0));
+        List<String> chapterContents = contents.createContents(chapters.get(0));
+
+        assertEquals(30, tom10Contents.size());
+        assertEquals(subCategoryTom10[0].trim(), tom10Contents.get(0));
+        assertEquals(subCategoryTom10[10].trim(), tom10Contents.get(10));
+        assertEquals(subCategoryTom10[29].trim(), tom10Contents.get(29));
+        assertEquals(8, sectionIIContents.size());
+        assertEquals(subCategorySectionII[0].trim(), sectionIIContents.get(0));
+        assertEquals(subCategorySectionII[1].trim(), sectionIIContents.get(1));
+        assertEquals(subCategorySectionII[7].trim(), sectionIIContents.get(7));
+        assertEquals(117, chapterContents.size());
     }
 }
