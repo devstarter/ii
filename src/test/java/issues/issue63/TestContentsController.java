50e7ef1cc230a3998aad82f2e93dd34a8008a536
Sllouyssgort <sllouyssgort@gmail.com>
2014-10-11 21:44:12 +0300

    contents test corrections

--- src/test/java/issues/issue63/TestContentsController.java
+++ src/test/java/issues/issue63/TestContentsController.java
@@ -2,6 +2,9 @@ package issues.issue63;
 
 
 import org.ayfaar.app.IntegrationTest;
+import org.ayfaar.app.model.Category;
+import org.ayfaar.app.model.Item;
+import org.ayfaar.app.utils.UriGenerator;
 import org.ayfaar.app.utils.contents.CategoryPresentation;
 import org.ayfaar.app.utils.contents.ContentsHelper;
 import org.junit.Test;
@@ -10,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
 
 public class TestContentsController extends IntegrationTest {
     @Autowired
@@ -26,11 +28,12 @@ public class TestContentsController extends IntegrationTest {
         assertTrue(contents.get(0).getDescription().isEmpty());
         assertEquals("Том 10", contents.get(0).getName());
         assertEquals(6, sections.size());
-        assertEquals("БДК / Раздел I", sections.get(0).getName());
-        assertEquals("БДК / Раздел V", sections.get(4).getName());
+        assertEquals("Раздел I", sections.get(0).getName());
+        assertEquals("Раздел V", sections.get(4).getName());
         assertEquals(5, chapters.size());
-        assertEquals("БДК / Раздел IV / Глава 1", chapters.get(0).getName());
-        assertEquals("БДК / Раздел IV / Глава 5", chapters.get(4).getName());
+        assertEquals("Глава 1", chapters.get(0).getName());
+        assertEquals("Глава 5", chapters.get(4).getName());
+        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел IV / Глава 5"), chapters.get(4).getUri());
         assertTrue(chapters.get(0).getChildren().isEmpty());
     }
 
@@ -41,10 +44,10 @@ public class TestContentsController extends IntegrationTest {
         List<CategoryPresentation> paragraphs = chapters.get(2).getChildren();
 
         assertEquals(1, contents.size());
-        assertEquals("БДК / Раздел III", contents.get(0).getName());
+        assertEquals("Раздел III", contents.get(0).getName());
         assertEquals(7, chapters.size());
-        assertEquals("БДК / Раздел III / Глава 1", chapters.get(0).getName());
-        assertEquals("БДК / Раздел III / Глава 7", chapters.get(6).getName());
+        assertEquals("Глава 1", chapters.get(0).getName());
+        assertEquals("Глава 7", chapters.get(6).getName());
         assertEquals(9, paragraphs.size());
         assertEquals("Параграф 10.3.3.1", paragraphs.get(0).getName());
         assertEquals("Параграф 10.3.3.9", paragraphs.get(8).getName());
@@ -58,12 +61,13 @@ public class TestContentsController extends IntegrationTest {
         List<CategoryPresentation> items = paragraphs.get(2).getChildren();
 
         assertEquals(1, contents.size());
-        assertEquals("БДК / Раздел IV / Глава 3", contents.get(0).getName());
+        assertEquals("Глава 3", contents.get(0).getName());
         assertEquals(16, paragraphs.size());
         assertEquals("Параграф 10.4.3.1", paragraphs.get(0).getName());
         assertEquals("Параграф 10.4.3.16", paragraphs.get(15).getName());
         assertEquals(16, paragraphs.size());
         assertEquals("10.11155", items.get(0).getName());
+        assertEquals(UriGenerator.generate(Item.class, "10.11155"), items.get(0).getUri());
         assertEquals("10.11170", items.get(15).getName());
         assertNull(items.get(0).getChildren());
     }
