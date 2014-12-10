efd0668693ee3429a6bf775bf7b0c2a6a7512bc9
Yurii <trohumchyk@gmail.com>
2014-12-10 17:16:29 +0200

    add url

--- src/main/java/org/ayfaar/app/controllers/search/cache/CacheTable.java
+++ src/main/java/org/ayfaar/app/controllers/search/cache/CacheTable.java
@@ -2,14 +2,20 @@ package org.ayfaar.app.controllers.search.cache;
 
 
 import org.ayfaar.app.dao.CommonDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.inject.Inject;
 import java.util.List;
 
+@Controller
+@RequestMapping("api/cache")
 public class CacheTable {
 
     @Inject CommonDao commonDao;
 
+
+    @RequestMapping("clear")
     public  void clearCacheTable(){
 
     List<CacheEntity> cacheEntities = commonDao.getAll(CacheEntity.class);
