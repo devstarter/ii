43e99779906890c0ad8daacfde382d6e822eb86a
Ekvi <dima_sh84@mail.ru>
2014-12-15 16:13:45 +0200

    refactored some tests

--- src/test/java/issues/issue63/CategoryControllerTest.java
+++ src/test/java/issues/issue63/CategoryControllerTest.java
@@ -26,53 +26,53 @@ public class CategoryControllerTest extends IntegrationTest {
 
         assertEquals("Том 10", rootCategory.getName());
         assertEquals(6, sections.size());
-        assertEquals("Раздел I", sections.get(0).getName());
+        assertEquals("Раздел I", sections.get(0).getName());
         assertEquals("Раздел V", sections.get(4).getName());
         assertEquals(5, chapters.size());
         assertEquals("Глава 1", chapters.get(0).getName());
         assertEquals("Глава 5", chapters.get(4).getName());
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел IV / Глава 5"), chapters.get(4).getUri());
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел IV/Глава 5"), chapters.get(4).getUri());
         assertNull(chapters.get(0).getChildren());
     }
 
     @Test
     public void testCreateContentsForSection() {
-        CategoryPresentation rootCategory = contentsHelper.createContents("БДК / Раздел III");
+        CategoryPresentation rootCategory = contentsHelper.createContents("БДК/Раздел III");
         List<CategoryPresentation> chapters = rootCategory.getChildren();
         List<CategoryPresentation> paragraphs = chapters.get(2).getChildren();
 
-        assertEquals("Раздел III", rootCategory.getName());
+        assertEquals("Раздел III", rootCategory.getName());
         assertEquals(7, chapters.size());
         assertEquals("Глава 1", chapters.get(0).getName());
         assertEquals("Глава 7", chapters.get(6).getName());
         assertEquals(9, paragraphs.size());
-        assertEquals("Параграф 10.3.3.1", paragraphs.get(0).getName());
-        assertEquals("Параграф 10.3.3.9", paragraphs.get(8).getName());
+        assertEquals("10.3.3.1", paragraphs.get(0).getName());
+        assertEquals("10.3.3.9", paragraphs.get(8).getName());
         assertNull(paragraphs.get(0).getChildren());
     }
 
     @Test
     public void testCreateContentsForChapter() {
-        String chapterFullName = "БДК / Раздел IV / Глава 3";
+        String chapterFullName = "БДК/Раздел IV/Глава 3";
         CategoryPresentation rootCategory = contentsHelper.createContents(chapterFullName);
         List<CategoryPresentation> paragraphs = rootCategory.getChildren();
 
         assertEquals("Глава 3", rootCategory.getName());
         assertEquals(UriGenerator.generate(Category.class, chapterFullName), rootCategory.getUri());
         assertEquals(16, paragraphs.size());
-        assertEquals("Параграф 10.4.3.1", paragraphs.get(0).getName());
-        assertEquals("Параграф 10.4.3.16", paragraphs.get(15).getName());
+        assertEquals("10.4.3.1", paragraphs.get(0).getName());
+        assertEquals("10.4.3.16", paragraphs.get(15).getName());
         assertEquals(16, paragraphs.size());
         assertNull(paragraphs.get(0).getChildren());
     }
 
     @Test
     public void testCreateContentsForParagraph() {
-        String paragraphName = "Параграф 10.1.2.3";
+        String paragraphName = "параграф:10.1.2.3";
         CategoryPresentation presentation = contentsHelper.createContents(paragraphName);
         List<CategoryPresentation> items = presentation.getChildren();
 
-        assertEquals("Параграф 10.1.2.3", presentation.getName());
+        assertEquals("10.1.2.3", presentation.getName());
         assertEquals(UriGenerator.generate(Category.class, paragraphName), presentation.getUri());
         assertNotNull(presentation.getChildren());
         assertEquals(10, items.size());
@@ -81,21 +81,22 @@ public class CategoryControllerTest extends IntegrationTest {
         assertTrue(items.get(0).getContent().startsWith("Так обычному «человеку» очень сложно представить себе,"));
         assertEquals("10.10140", items.get(9).getName());
         assertTrue(items.get(9).getContent().startsWith("А это означает, что с помощью сосредоточенного внимания,"));
-        assertEquals(UriGenerator.generate(Category.class, "Параграф 10.1.3.1"), presentation.getNext());
-        assertEquals(UriGenerator.generate(Category.class, "Параграф 10.1.2.2"), presentation.getPrevious());
+        assertEquals(UriGenerator.generate(Category.class, "параграф:10.1.3.1"), presentation.getNext());
+        assertEquals(UriGenerator.generate(Category.class, "параграф:10.1.2.2"), presentation.getPrevious());
         assertNotNull(presentation.getParents());
     }
 
     @Test
     public void testCreateContentsWithParents() {
-        CategoryPresentation rootCategory = contentsHelper.createContents("БДК / Раздел IV / Глава 5");
+        CategoryPresentation rootCategory = contentsHelper.createContents("БДК/Раздел IV/Глава 5");
         List<CategoryPresentation> parents = rootCategory.getParents();
         List<CategoryPresentation> chapterChildren = rootCategory.getChildren();
 
-        assertEquals(3, parents.size());
-        assertEquals("Раздел IV", parents.get(0).getName());
+        assertEquals(4, parents.size());
+        assertEquals("Раздел IV", parents.get(0).getName());
         assertEquals("Том 10", parents.get(1).getName());
         assertEquals("БДК", parents.get(2).getName());
+        assertEquals("Содержание", parents.get(3).getName());
         assertNull(parents.get(0).getParents());
         assertNull(parents.get(0).getChildren());
         assertNull(chapterChildren.get(0).getParents());
@@ -103,16 +104,16 @@ public class CategoryControllerTest extends IntegrationTest {
 
     @Test
     public void testNextAndPreviousCategory() {
-        CategoryPresentation rootCategory = contentsHelper.createContents("БДК / Раздел III / Глава 7");
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел III / Глава 6"), rootCategory.getPrevious());
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел IV / Глава 1"), rootCategory.getNext());
+        CategoryPresentation rootCategory = contentsHelper.createContents("БДК/Раздел III/Глава 7");
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел III/Глава 6"), rootCategory.getPrevious());
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел IV/Глава 1"), rootCategory.getNext());
     }
 
     @Test
     public void testCreateContentsWhenNextOrPreviousCategoryIsNull() {
         CategoryPresentation tom10Content = contentsHelper.createContents("Том 10");
         CategoryPresentation tom14Content = contentsHelper.createContents("Том 14");
-        CategoryPresentation section = contentsHelper.createContents("БДК / Раздел ХVIII");
+        CategoryPresentation section = contentsHelper.createContents("БДК/Раздел ХVIII");
 
         assertNull(tom10Content.getPrevious());
         assertEquals(UriGenerator.generate(Category.class, "Том 14"), tom10Content.getNext());
--- src/test/java/org/ayfaar/app/utils/CategoryMapImplIntegrationTest.java
+++ src/test/java/org/ayfaar/app/utils/CategoryMapImplIntegrationTest.java
@@ -16,41 +16,42 @@ public class CategoryMapImplIntegrationTest extends IntegrationTest{
 
     @Test
     public void testGetChildrenForSection() {
-        CategoryMap.CategoryProvider provider = categoryMap.getProviderForCategory("БДК / Раздел I");
+        CategoryMap.CategoryProvider provider = categoryMap.getProviderForCategory("БДК/Раздел I");
         List<CategoryMap.CategoryProvider> children = provider.getChildren();
 
         assertEquals(4, children.size());
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел I / Глава 1"), children.get(0).getUri());
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел I / Глава 4"), children.get(3).getUri());
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел I/Глава 1"), children.get(0).getUri());
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел I/Глава 4"), children.get(3).getUri());
     }
 
     @Test
     public void testGetParentForParagraph() {
-        CategoryMap.CategoryProvider provider = categoryMap.getProviderForCategory("Параграф 10.1.1.1");
+        CategoryMap.CategoryProvider provider = categoryMap.getProviderForCategory("параграф:10.1.1.1");
         CategoryMap.CategoryProvider parent = provider.getParent();
 
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел I / Глава 1"), parent.getUri());
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел I/Глава 1"), parent.getUri());
     }
 
     @Test
     public void testGetParentsFotParagraph() {
-        CategoryMap.CategoryProvider provider = categoryMap.getProviderForCategory("Параграф 10.1.1.1");
+        CategoryMap.CategoryProvider provider = categoryMap.getProviderForCategory("параграф:10.1.1.1");
         List<CategoryMap.CategoryProvider> parents = provider.getParents();
 
-        assertEquals(4, parents.size());
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел I / Глава 1"), parents.get(0).getUri());
-        assertEquals(UriGenerator.generate(Category.class, "БДК / Раздел I"), parents.get(1).getUri());
+        assertEquals(5, parents.size());
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел I/Глава 1"), parents.get(0).getUri());
+        assertEquals(UriGenerator.generate(Category.class, "БДК/Раздел I"), parents.get(1).getUri());
         assertEquals(UriGenerator.generate(Category.class, "Том 10"), parents.get(2).getUri());
         assertEquals(UriGenerator.generate(Category.class, "БДК"), parents.get(3).getUri());
+        assertEquals(UriGenerator.generate(Category.class, "Содержание"), parents.get(4).getUri());
     }
 
     @Test
     public void testGetProviderByItemNumber() {
         CategoryMap.CategoryProvider provider = categoryMap.getProviderByItemNumber("10.10037");
-        assertEquals(UriGenerator.generate(Category.class, "Параграф 10.1.1.4"), provider.getUri());
+        assertEquals(UriGenerator.generate(Category.class, "параграф:10.1.1.4"), provider.getUri());
         provider = categoryMap.getProviderByItemNumber("14.15639");
-        assertEquals(UriGenerator.generate(Category.class, "Параграф 14.16.2.14"), provider.getUri());
+        assertEquals(UriGenerator.generate(Category.class, "параграф:14.16.2.14"), provider.getUri());
         provider = categoryMap.getProviderByItemNumber("14.15598");
-        assertEquals(UriGenerator.generate(Category.class, "Параграф 14.16.2.4"), provider.getUri());
+        assertEquals(UriGenerator.generate(Category.class, "параграф:14.16.2.4"), provider.getUri());
     }
 }
