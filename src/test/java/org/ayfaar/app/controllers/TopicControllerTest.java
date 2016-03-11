7fd7bcaf314b23d6a0e8a71c895b103545878ce4
Sllouyssgort <sllouyssgort@gmail.com>
2016-03-12 01:45:38 +0300

    test review

--- src/test/java/org/ayfaar/app/controllers/TopicControllerTest.java
+++ src/test/java/org/ayfaar/app/controllers/TopicControllerTest.java
@@ -16,49 +16,51 @@ import static com.jayway.restassured.RestAssured.when;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("dev")
 @SpringApplicationConfiguration(Application.class)
-@WebIntegrationTest(randomPort = true)
+@WebIntegrationTest
 public class TopicControllerTest {
     @Test
     public void testGet() {
+        when().
+                get("/api/topic/{name}/add-child/{child}", "1", "2").
+        then()
+                .log().all().
+                statusCode(HttpStatus.SC_OK);
+
+        // todo добавить ещё чайлда "3" и парента "4"
 
         when().
-                get("/api/topic/{name}/add-child/{child}", "1","2").then().log().all();
-        when().
-                get("/api/topic/{name}", "1").
-                then().
-                log().all().
-                statusCode(HttpStatus.SC_OK).
-
-                body("name", Matchers.is("1")).
-                body("uri", Matchers.is("тема:1")).
-                body("children", Matchers.hasItems("3", "2")).
-                body("parents", Matchers.hasItem("4"))
+            get("/api/topic/{name}", "1").
+        then().
+            log().all().
+            statusCode(HttpStatus.SC_OK).
+            body("name", Matchers.is("1")).
+            body("uri", Matchers.is("тема:1")).
+            body("children", Matchers.hasItems("3", "2")).
+            body("parents", Matchers.hasItem("4"))
         ;
     }
 
     @Test
-
     public void testAddFor() {
-                given().
-                    parameters("name", "19999", "uri", "тема:19999").
-                when().
-                    post("/api/topic/for").
-                then().
-                        log().all().
-                        statusCode(HttpStatus.SC_OK)
-
+        given().
+            contentType("application/x-www-form-urlencoded; charset=UTF-8"). // это чтобы русский понимал в параметрах
+            param("name", "1"). // имя темы
+            param("uri", "видео:youtube:_8vYBLrOq-w"). // uri объекта к которому прилинковать тему
+        when().
+            post("/api/topic/for").
+        then().
+                log().all().
+                statusCode(HttpStatus.SC_OK)
         ;
     }
 
     @Test
     public void testAddChild() {
-
         when().
-                get("/api/topic/{name}/add-child/{child}", "1", "2").
-                then().
-                log().all().
-                statusCode(HttpStatus.SC_OK)
-
+            get("/api/topic/{name}/add-child/{child}", "1", "2").
+        then().
+            log().all().
+            statusCode(HttpStatus.SC_OK)
         ;
     }
 
@@ -66,13 +68,14 @@ public class TopicControllerTest {
 
     @Test
     public void testUnLink() {
+        // сначала прилинковываем
+        when().get("/api/topic/{name}/add-child/{child}", "1", "2").then().statusCode(HttpStatus.SC_OK);
 
         when().
-                get("/api/topic/{name}/unlink/{linked}", "1", "2").
-                then().
-                log().all().
-                statusCode(HttpStatus.SC_OK)
-
+            get("/api/topic/{name}/unlink/{linked}", "1", "2").
+        then().
+            log().all().
+            statusCode(HttpStatus.SC_OK)
         ;
     }
 }
