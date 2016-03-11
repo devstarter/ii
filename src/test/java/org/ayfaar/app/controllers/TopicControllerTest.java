d480ee1b6115fb614a7692146778559d77effa77
Den Street <masloed@yandex.ru>
2016-03-11 23:31:23 +0200

    topicController test

--- src/test/java/org/ayfaar/app/controllers/TopicControllerTest.java
+++ src/test/java/org/ayfaar/app/controllers/TopicControllerTest.java
@@ -10,16 +10,19 @@ import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static com.jayway.restassured.RestAssured.given;
 import static com.jayway.restassured.RestAssured.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("dev")
 @SpringApplicationConfiguration(Application.class)
-@WebIntegrationTest
+@WebIntegrationTest(randomPort = true)
 public class TopicControllerTest {
     @Test
     public void testGet() {
 
+        when().
+                get("/api/topic/{name}/add-child/{child}", "1","2").then().log().all();
         when().
                 get("/api/topic/{name}", "1").
                 then().
@@ -30,6 +33,46 @@ public class TopicControllerTest {
                 body("uri", Matchers.is("тема:1")).
                 body("children", Matchers.hasItems("3", "2")).
                 body("parents", Matchers.hasItem("4"))
+        ;
+    }
+
+    @Test
+
+    public void testAddFor() {
+                given().
+                    parameters("name", "19999", "uri", "тема:19999").
+                when().
+                    post("/api/topic/for").
+                then().
+                        log().all().
+                        statusCode(HttpStatus.SC_OK)
+
+        ;
+    }
+
+    @Test
+    public void testAddChild() {
+
+        when().
+                get("/api/topic/{name}/add-child/{child}", "1", "2").
+                then().
+                log().all().
+                statusCode(HttpStatus.SC_OK)
+
+        ;
+    }
+
+
+
+    @Test
+    public void testUnLink() {
+
+        when().
+                get("/api/topic/{name}/unlink/{linked}", "1", "2").
+                then().
+                log().all().
+                statusCode(HttpStatus.SC_OK)
+
         ;
     }
 }
