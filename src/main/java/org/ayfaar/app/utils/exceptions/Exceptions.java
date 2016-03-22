95d35397dd36275008728151092eadc9be9080f7
Sllouyssgort <sllouyssgort@gmail.com>
2016-03-22 08:47:52 +0300

    add logical exception ITEM_NOT_FOUND

--- src/main/java/org/ayfaar/app/utils/exceptions/Exceptions.java
+++ src/main/java/org/ayfaar/app/utils/exceptions/Exceptions.java
@@ -2,7 +2,8 @@ package org.ayfaar.app.utils.exceptions;
 
 public enum Exceptions {
 
-    TOPIC_NOT_FOUND("Topic for " + "%s" + " not found");
+    TOPIC_NOT_FOUND("Topic for %s not found"),
+    ITEM_NOT_FOUND("Item %s not found");
     //OTHER_CODE("Description");
 
 
