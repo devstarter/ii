18670d31157e4bdb717325c8b818143e00aa8927
Sllouyssgort <sllouyssgort@gmail.com>
2016-04-12 23:10:53 +0300

    bug in AdvanceComparator

--- src/test/java/org/ayfaar/app/utils/AdvanceComparatorTest.java
+++ src/test/java/org/ayfaar/app/utils/AdvanceComparatorTest.java
@@ -1,17 +1,19 @@
 package org.ayfaar.app.utils;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AdvanceComparatorTest {
 
     @Test
     public void compare() {
-        final List<String> list = Arrays.asList(
+        final List<String> list = asList(
                 "Как всё появилось. Часть 14. Пространственно-Временные Континуумы. Планетарный ПВК",
                 "Как всё появилось. Часть 6. Образование Фокусов Самосознания и Резомиралов",
                 "Как всё появилось. Часть 1. Текст для примера",
@@ -24,12 +26,12 @@ public class AdvanceComparatorTest {
         System.out.println(list.get(2));
         System.out.println(list.get(3));
 
-        Assert.assertTrue(list.get(0).contains("Часть 1."));
-        Assert.assertTrue(list.get(1).contains("Часть 6."));
-        Assert.assertTrue(list.get(2).contains("Часть 10."));
-        Assert.assertTrue(list.get(3).contains("Часть 14."));
+        assertTrue(list.get(0).contains("Часть 1."));
+        assertTrue(list.get(1).contains("Часть 6."));
+        assertTrue(list.get(2).contains("Часть 10."));
+        assertTrue(list.get(3).contains("Часть 14."));
 
-        final List<String> list1 = Arrays.asList(
+        final List<String> list1 = asList(
                 "тект 1. текст 10. текст",
                 "тект 1. текст 14. текст",
                 "тект 1. текст 1. текст");
@@ -40,8 +42,17 @@ public class AdvanceComparatorTest {
         System.out.println(list1.get(1));
         System.out.println(list1.get(2));
 
-        Assert.assertTrue(list1.get(0).contains(" 1."));
-        Assert.assertTrue(list1.get(1).contains(" 10."));
-        Assert.assertTrue(list1.get(2).contains(" 14."));
+        assertTrue(list1.get(0).contains(" 1."));
+        assertTrue(list1.get(1).contains(" 10."));
+        assertTrue(list1.get(2).contains(" 14."));
+    }
+
+    @Test
+    public void testNumberTail() {
+        final List<String> list = asList("12.9.1.1", "12.9.1.2", "12.9.1.10");
+        list.sort(AdvanceComparator.INSTANCE);
+        assertEquals("12.9.1.1", list.get(0));
+        assertEquals("12.9.1.2", list.get(1));
+        assertEquals("12.9.1.10", list.get(2));
     }
 }
