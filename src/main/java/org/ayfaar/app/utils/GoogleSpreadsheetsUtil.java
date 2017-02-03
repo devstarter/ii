166393180998c1c3aab12b3037a6f4fae2065b39
Oleh Pinta <oleg.sabfir@gmail.com>
2017-02-04 01:23:14 +0200

    OAuth 2.0 for Service Accounts by https://developers.google.com/identity/protocols/OAuth2ServiceAccount

--- src/main/java/org/ayfaar/app/utils/GoogleSpreadsheetsUtil.java
+++ src/main/java/org/ayfaar/app/utils/GoogleSpreadsheetsUtil.java
@@ -1,54 +1,37 @@
 package org.ayfaar.app.utils;
 
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Collections;
+import java.util.Set;
 
 @Slf4j
 public class GoogleSpreadsheetsUtil {
 	/** Application name. */
 	private static final String APPLICATION_NAME = "Spreadsheets ii";
 
-	/** Directory to store user credentials for this application. */
-	private static final java.io.File DATA_STORE_DIR = new java.io.File(
-			"credentials/sheets_googleapis_com");
+    private static final String ACCOUNT_PRIVATE_KEY = "/account-private-key-google-api-devstarter.json";
 
 	/** Global instance of the JSON factory. */
 	private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-	/** Global instance of the scopes required by this quickstart.
-	 *
-	 * If modifying these scopes, delete your previously saved credentials
-	 * at ~/.credentials/sheets.googleapis.com-java-quickstart
-	 */
-	private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS);
-
-	/** Global instance of the {@link FileDataStoreFactory}. */
-	private static FileDataStoreFactory DATA_STORE_FACTORY;
+	/** Global instance of the scopes. */
+    private static final Set<String> SCOPES = Collections.singleton(SheetsScopes.SPREADSHEETS);
 
 	/** Global instance of the HTTP transport. */
 	private static HttpTransport HTTP_TRANSPORT;
-
 	static {
 		try {
 			HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-			DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
 		} catch (Throwable t) {
 			log.error(t.getMessage(), t);
 		}
@@ -60,23 +43,16 @@ public class GoogleSpreadsheetsUtil {
 	 * @throws IOException
 	 */
 	private static Credential authorize() throws IOException {
-		// Load client secrets.
-		InputStream in =
-				GoogleSpreadsheetsUtil.class.getResourceAsStream("/client_secret.json");
-		GoogleClientSecrets clientSecrets =
-				GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        GoogleCredential credential = null;
+        try {
+            credential = GoogleCredential
+                    .fromStream(GoogleSpreadsheetsUtil.class.getResourceAsStream(ACCOUNT_PRIVATE_KEY))
+                    .createScoped(SCOPES);
+        } catch (IOException e) {
+            log.error("Can't create Google credential", e);
+        }
 
-		// Build flow and trigger user authorization request.
-		GoogleAuthorizationCodeFlow flow =
-				new GoogleAuthorizationCodeFlow.Builder(
-						HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-						.setDataStoreFactory(DATA_STORE_FACTORY)
-						.setAccessType("offline")
-						.build();
-		Credential credential = new AuthorizationCodeInstalledApp(
-				flow, new LocalServerReceiver()).authorize("user");
-		log.debug("Credentials saved to {}" + DATA_STORE_DIR.getAbsolutePath());
-		return credential;
+        return credential;
 	}
 
 	/**
