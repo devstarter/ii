706bcaed1c9f936c29379094820448aa5d38c4ac
sllouyssgort <sllouyssgort@gmail.com>
2019-10-03 12:52:59 +0300

    +

--- src/main/java/org/ayfaar/app/controllers/SslController.kt
+++ src/main/java/org/ayfaar/app/controllers/SslController.kt
@@ -10,5 +10,5 @@ class SslController {
 
     @ResponseBody
     @RequestMapping(".well-known/acme-challenge/{id}")
-    fun authenticate(@PathVariable id: String) = "$id.CzwDj3G70ZUXvo9joO4OeSfe01Iou6l1_RUhv4pc2mc"
+    fun authenticate(@PathVariable id: String) = "$id.D9BNMjYsHutrxi_T8Ib1BDBt2R3kXzsnu2_S3tu6aTI" //"3MDRqPVXLHN63zsNO_obi0Ww9RzG3G-h5Q4YcY96f7M.D9BNMjYsHutrxi_T8Ib1BDBt2R3kXzsnu2_S3tu6aTI"
 }
--- src/main/java/org/ayfaar/app/controllers/VocabularyController.kt
+++ src/main/java/org/ayfaar/app/controllers/VocabularyController.kt
@@ -1,5 +1,6 @@
 package org.ayfaar.app.controllers
 
+import com.google.common.net.UrlEscapers
 import org.ayfaar.app.vocabulary.VocabularyService
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import javax.inject.Inject
+import javax.servlet.http.HttpServletResponse
 
 
 @RestController
@@ -17,16 +19,22 @@ import javax.inject.Inject
 class VocabularyController {
     @Inject lateinit var service: VocabularyService
 
+    private val name = UrlEscapers.urlFragmentEscaper().escape("терминологический словарь ииссиидиологических неологизмов")
+
     @RequestMapping("doc")
-    fun getDoc(): ResponseEntity<ByteArray> {
+    fun getDoc(response: HttpServletResponse) = response.sendRedirect("/api/vocabulary/doc/$name.${getCurrentDate()}.docx")
 
+    private fun getCurrentDate() = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))
+
+    @RequestMapping("doc/{name:.+}")
+    fun getDocNamed(): ResponseEntity<ByteArray> {
         val file = service.getDoc()
-
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_OCTET_STREAM
         // Here you have to set the actual filename of your pdf
-        val filename = "словарь.${LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))}.docx"
-        headers.setContentDispositionFormData(filename, filename)
+//        val filename = "словарь.${LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd"))}.docx"
+//        headers.setContentDispositionFormData(filename, filename)
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment;")
         return ResponseEntity(file.readBytes(), headers, HttpStatus.OK)
     }
 }
